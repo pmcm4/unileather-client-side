2bf32f9dc5d42ec955abf587e322a96bc07b796e
Paolo Morato <pmorato187@gmail.com>
2022-06-17 10:56:56 +0800

    change api link on product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -21,8 +21,8 @@ const Products = ({cat, filters, sort}) => {
       try{
         const res = await axiosInstance.get(
           cat 
-          ? `http://localhost:5000/api/products?category=${cat}` 
-          : "http://localhost:5000/api/products"
+          ? `https://unileather-api.herokuapp.com/api/products?category=${cat}` 
+          : "https://unileather-api.herokuapp.com/api/products"
           );
           setProducts(res.data);
       }catch(err){
